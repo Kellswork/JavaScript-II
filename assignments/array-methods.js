bb840ba6002575aefd400e2408872b0dc62b51a2
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-07 15:44:30 +0100

    solution for challenge 5

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -60,7 +60,7 @@ let fullName = [];
      fullName.push(`${runner.first_name} ${runner.last_name}`);
  });
 
-console.log(fullName);
+//console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
@@ -69,7 +69,7 @@ allcaps = runners.map(runner => {
  return runner.first_name.toLocaleUpperCase();
 });
 
-console.log(allCaps); 
+//console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
@@ -77,20 +77,34 @@ let largeShirts = [];
 largeShirts = runners.filter(runner => {
    return runner.shirt_size.toLowerCase().indexOf(('L').toLowerCase()) > -1;
 });
-console.log(largeShirts);
+//console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
 ticketPriceTotal = runners.reduce((total, currentValue) => total + currentValue.donation, 0);
 
-console.log(ticketPriceTotal);
+//console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// display name of althete's working withn skinix and skinte
+
+let workPlace = [];
+
+ runners.forEach(runner => {
+   if((runner.company_name === 'Skinix') || (runner.company_name === 'Skinte')) {
+ workPlace.push(`Name: ${runner.first_name} ${runner.last_name}, company: ${runner.company_name}`);
+   }
+})
+// console.log(workPlace);
 
 // Problem 2
-
+// display athletes that have made donations higher than 200;
+let higherDonations = [];
+higherDonations = runners.filter(runner => (runner.donation > 200));
+console.log(higherDonations);
 // Problem 3
+
